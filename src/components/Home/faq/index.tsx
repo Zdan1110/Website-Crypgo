1bc55545479de8a90cca4482dfa8fc05ab67142a
Zdan1110 <Ziddanda@gmail.com>
2025-10-26 20:51:28 +0700

    FAQ3

--- src/components/Home/faq/index.tsx
+++ src/components/Home/faq/index.tsx
@@ -1,7 +1,3 @@
-"use client";
-import Image from "next/image";
-import { motion } from "framer-motion";
-import { getImagePrefix } from "@/utils/utils";
 import { useState } from "react";
 
 const faqs = [
